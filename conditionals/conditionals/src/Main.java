cf0bcb648192358a511b19512a7d903933b7c23c
MertKx <113070286+MertKx@users.noreply.github.com>
2023-03-23 15:55:12 +0300

    Add files via upload

---
+++ conditionals/conditionals/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int sayim= 74;
+        if (sayim < 78) {
+            System.out.println("Sayınız 78den küçükmüş");
+        }else if(sayim == 78){
+            System.out.println("Sayınız 78e eşitmiş");
+        }else {
+            System.out.println("Sayı 78den büyük");
+        }
+    }
+}
---
+++ datatypes/datatypes/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayim = 2 ;
+        sayim = 8;
+        System.out.println(sayim);
+    }
+}
