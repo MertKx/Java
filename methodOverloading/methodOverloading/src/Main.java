f7d9901842c312fea0695fee4ebb295c9b5bde9b
Cafer Mert Kalay <113070286+MertKx@users.noreply.github.com>
2024-02-21 21:07:26 +0300

    Add files via upload

---
+++ methodOverloading/methodOverloading/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    //OVERLOADİNG ÖRNEĞİ
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.topla(6,9));
+        System.out.println(dortIslem.topla(4,6,7));
+    }
+}
